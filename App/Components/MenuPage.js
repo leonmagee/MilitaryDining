4242c39a3267ea096d5d727986fafb864a922ad2
Leon Magee <leonmagee@hotmail.com>
2017-10-19 14:23:47 -0700

    update

--- App/Components/MenuPage.js
+++ App/Components/MenuPage.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
-import {View, Text, StyleSheet} from 'react-native'
+import {View, ScrollView, Text, StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
   mainWrap: {
-    backgroundColor: 'blue',
+    //backgroundColor: 'blue',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   },
   mainText: {
     fontSize: 22,
-    color: '#FFF',
+    color: '#555',
     fontWeight: 'bold'
   },
   messHallWrap: {
@@ -40,22 +40,57 @@ class MenuPage extends Component {
     //   )
     // })
     const menuz = this.props.currentMenu;
-    console.log('menuzzzzz',menuz)
+
+    const menu_days = menuz.menus.map((menu, key) => {
+
+      let breakfast = menu.breakfast.map((item, key) => {
+        return (
+          <Text key={key}>{item}</Text>
+        )
+      })
+
+      let lunch = menu.lunch.map((item, key) => {
+        return (
+          <Text key={key}>{item}</Text>
+        )
+      })
+
+      let dinner = menu.dinner.map((item, key) => {
+        return (
+          <Text key={key}>{item}</Text>
+        )
+      })
+
+      let pastry_bar = menu.pastry_bar.map((item, key) => {
+        return (
+          <Text key={key}>{item}</Text>
+        )
+      })
+
+      return (
+        <View key={key}>
+          <Text>Day {menu.day}</Text>
+          <Text>Breakfast</Text>
+          {breakfast}
+          <Text>Lunch</Text>
+          {lunch}
+          <Text>Dinner</Text>
+          {dinner}
+          <Text>Pastry Bar</Text>
+          {pastry_bar}
+        </View>
+      )
+    })
 
     return (
-      <View style={styles.mainWrap}>
-        <Text style={styles.mainText}>{menuz.name}</Text>
-        <View style={styles.messHallWrap}>
-          <Text>Monday</Text>
-          <Text>Breakfast: {menuz.menus.monday.breakfast}</Text>
-          <Text>Lunch: {menuz.menus.monday.lunch}</Text>
-          <Text>Dinner: {menuz.menus.monday.dinner}</Text>
-          <Text>tuesday</Text>
-          <Text>Breakfast: {menuz.menus.tuesday.breakfast}</Text>
-          <Text>Lunch: {menuz.menus.tuesday.lunch}</Text>
-          <Text>Dinner: {menuz.menus.tuesday.dinner}</Text>
+      <ScrollView>
+        <View style={styles.mainWrap}>
+          <Text style={styles.mainText}>{menuz.name}</Text>
+          <View style={styles.messHallWrap}>
+            {menu_days}
+          </View>
         </View>
-      </View>
+      </ScrollView>
     )
   }
 }
--- App/Data/Data.js
+++ App/Data/Data.js
@@ -1,8 +1,9 @@
 const SampleData = [
   {
     name: 'H 3120 Male Recruit',
-    menus: {
+    menus: [
       {
+        day: '1',
         breakfast: [
           'ASSORTED DRY CEREAL',
           'HOMINY GRITS',
@@ -14,7 +15,7 @@ const SampleData = [
           'POTATO O\'BRIEN',
           'STEAMED RICE',
           'PANCAKES'
-      ],
+        ],
         lunch: [
           'MINESTRONE SOUP',
           'BEEF SHORT RIBS',
@@ -33,36 +34,115 @@ const SampleData = [
           'BROCCOLI POLONAISE',
           'BROWN GRAVY'
         ],
-        pastry_bar: [
-          'CINNAMON ROLL',
-          'DANISH, ASSORTED',
-          'QUICK COFFEE CAKE',
-          'CHOCOLATE CHIP COOKIES',
-          'CHERRY COBBLER'
-        ]
-      },
+        pastry_bar: ['CINNAMON ROLL', 'DANISH, ASSORTED', 'QUICK COFFEE CAKE', 'CHOCOLATE CHIP COOKIES', 'CHERRY COBBLER']
+      }, {
+        day: '2',
+        breakfast: [
+          'ASSORTED DRY CEREAL',
+          'HOMINY GRITS',
+          'SCRAMBLED EGGS',
+          'HARD COOKED EGGS',
+          'OVEN FRIED BACON',
+          'BAKED SAUSAGE LINKS, BEEF',
+          'CREAMED GROUND BEEF',
+          'POTATO O\'BRIEN',
+          'STEAMED RICE',
+          'PANCAKES'
+        ],
+        lunch: [
+          'MINESTRONE SOUP',
+          'BEEF SHORT RIBS',
+          'CHICKEN AND ITALIAN VEGETABLE PASTA',
+          'STEAMED RICE',
+          'ROASTED ZUCCHINI',
+          'CALIFORNIA BLEND'
+        ],
+        dinner: [
+          'CHICKEN GUMBO SOUP',
+          'BRAISED PORK CHOPS',
+          'TURKEY ALA KING',
+          'BUTTERED EGG NOODLE',
+          'ISLANDER\'S RICE',
+          'STEAMED BABY CARROTS',
+          'BROCCOLI POLONAISE',
+          'BROWN GRAVY'
+        ],
+        pastry_bar: ['CINNAMON ROLL', 'DANISH, ASSORTED', 'QUICK COFFEE CAKE', 'CHOCOLATE CHIP COOKIES', 'CHERRY COBBLER']
+      }
+    ]
+  }, {
+    name: 'H 3150 Male Recruit',
+    menus: [
       {
-        breakfast: 'bagel',
-        lunch: 'hot dogs',
-        dinner: 'chicken',
-      },
-    }
-  },
-  {
-    name: 'Stevens Mess Hall',
-    menus: {
-      monday: {
-        breakfast: 'toast',
-        lunch: 'salad',
-        dinner: 'pasta',
-      },
-      tuesday: {
-        breakfast: 'donuts',
-        lunch: 'tacos',
-        dinner: 'rice & beans',
-      },
-    }
+        day: '1',
+        breakfast: [
+          'ASSORTED DRY CEREAL',
+          'HOMINY GRITS',
+          'SCRAMBLED EGGS',
+          'HARD COOKED EGGS',
+          'OVEN FRIED BACON',
+          'BAKED SAUSAGE LINKS, BEEF',
+          'CREAMED GROUND BEEF',
+          'POTATO O\'BRIEN',
+          'STEAMED RICE',
+          'PANCAKES'
+        ],
+        lunch: [
+          'MINESTRONE SOUP',
+          'BEEF SHORT RIBS',
+          'CHICKEN AND ITALIAN VEGETABLE PASTA',
+          'STEAMED RICE',
+          'ROASTED ZUCCHINI',
+          'CALIFORNIA BLEND'
+        ],
+        dinner: [
+          'CHICKEN GUMBO SOUP',
+          'BRAISED PORK CHOPS',
+          'TURKEY ALA KING',
+          'BUTTERED EGG NOODLE',
+          'ISLANDER\'S RICE',
+          'STEAMED BABY CARROTS',
+          'BROCCOLI POLONAISE',
+          'BROWN GRAVY'
+        ],
+        pastry_bar: ['CINNAMON ROLL', 'DANISH, ASSORTED', 'QUICK COFFEE CAKE', 'CHOCOLATE CHIP COOKIES', 'CHERRY COBBLER']
+      }, {
+        day: '2',
+        breakfast: [
+          'ASSORTED DRY CEREAL',
+          'HOMINY GRITS',
+          'SCRAMBLED EGGS',
+          'HARD COOKED EGGS',
+          'OVEN FRIED BACON',
+          'BAKED SAUSAGE LINKS, BEEF',
+          'CREAMED GROUND BEEF',
+          'POTATO O\'BRIEN',
+          'STEAMED RICE',
+          'PANCAKES'
+        ],
+        lunch: [
+          'MINESTRONE SOUP',
+          'BEEF SHORT RIBS',
+          'CHICKEN AND ITALIAN VEGETABLE PASTA',
+          'STEAMED RICE',
+          'ROASTED ZUCCHINI',
+          'CALIFORNIA BLEND'
+        ],
+        dinner: [
+          'CHICKEN GUMBO SOUP',
+          'BRAISED PORK CHOPS',
+          'TURKEY ALA KING',
+          'BUTTERED EGG NOODLE',
+          'ISLANDER\'S RICE',
+          'STEAMED BABY CARROTS',
+          'BROCCOLI POLONAISE',
+          'BROWN GRAVY'
+        ],
+        pastry_bar: ['CINNAMON ROLL', 'DANISH, ASSORTED', 'QUICK COFFEE CAKE', 'CHOCOLATE CHIP COOKIES', 'CHERRY COBBLER']
+      }
+    ]
   }
+
 ]
 
 export default SampleData
