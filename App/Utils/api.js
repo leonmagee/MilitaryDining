caf1152b731527b3e794cc221bc418292de07def
Leon Magee <leonmagee33@gmail.com>
2017-12-12 08:17:13 -0800

    update

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -1,11 +1,11 @@
 var api = {
     getMenus() {
-      const menus_url = 'https://militarydining.wpengine.com/wp-json/md/menus?abcde'
+      const menus_url = 'https://militarydining.wpengine.com/wp-json/md/menus'
       return fetch(menus_url, {headers: {'Cache-Control': 'no-cache'}}).then((res) => res.json())
 
 
       // headers: {
-      //   'Cache-Control': 'no-cache'
+      //   'Cache-Control': 'no-cache
       // }
 
 
